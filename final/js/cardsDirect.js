5b715fed4ce5f862eb4a36e81d4febf3ca5bc5fd
gavin-cannon <gavincannon@byui.edu>
2022-07-20 14:37:51 -0600

    finished temple json page

--- final/js/cardsDirect.js
+++ final/js/cardsDirect.js
@@ -27,27 +27,36 @@ function buildTempleCards(data) {
         let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
         let p = document.createElement('p');
-        let likebutton = document.createElement('img')
+        let new_p = document.createElement('p');
+        let likebutton = document.createElement('img');
+        let history_p = document.createElement('p');
+
         h2.innerHTML = `${temple.name}`;
-        p.innerHTML = `Phone Number: ${temple.phone}<br> Address: ${temple.address}`;
-        portrait.setAttribute('src', `${temple.imageurl}`)
-        portrait.setAttribute('alt', `Portrait of ${temple.name} ${temple.lastname}`)
-        portrait.setAttribute('loading', 'lazy')
-        likebutton.setAttribute('src', `${temple.likebutton}`)
-        likebutton.setAttribute('onclick', 'like_button(this)')
-        likebutton.setAttribute('class', `like_btn${class_number}`)
+        p.innerHTML = `<h3>Phone Number:</h3> ${temple.phone}<br><br> <h3>Address:</h3> ${temple.address}<br><br> <h3>Email:</h3> ${temple.email}<br> <br><h3>Website:</h3> ${temple.website}`;
+        new_p.innerHTML = `<h3>Services:</h3> ${temple.services}<br><br> <h3>Session:</h3> ${temple.session}<br> <h3>Ordinances:</h3> ${temple.ordinances}<br> <br> <h3>Closures for 2022:</h3>  ${temple.closure}<br>`
+        history_p.innerHTML = `<h3>History:</h3>${temple.history}`;
+        portrait.setAttribute('src', `${temple.imageurl}`);
+        portrait.setAttribute('alt', `Portrait of ${temple.name} ${temple.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        likebutton.setAttribute('src', `${temple.likebutton}`);
+        likebutton.setAttribute('onclick', 'like_button(this)');
+        likebutton.setAttribute('class', `like_btn${class_number}`);
 
         if (Number(localStorage.getItem(`like_btn${class_number}`)) === 0) {
-            likebutton.setAttribute('src', 'images/heartred.png')
-            console.log("hi")
+            likebutton.setAttribute('src', 'images/heartred.png');
+            console.log("hi");
         } else {
-            likebutton.setAttribute('src', 'images/heart.png')
+            likebutton.setAttribute('src', 'images/heart.png');
         }
 
 
         card.append(h2);
         card.append(p);
+        card.append(new_p);
+        card.append(history_p);
         card.append(portrait);
+
+
         card.append(likebutton);
 
         cards.append(card);
@@ -57,20 +66,20 @@ function buildTempleCards(data) {
 
 function like_button(x) {
 
-    let like_class = x.getAttribute("class")
-    console.log(like_class)
+    let like_class = x.getAttribute("class");
+    console.log(like_class);
     let like_attribute = x.getAttribute("src");
 
     if (like_attribute === 'images/heart.png') {
-        x.setAttribute('src', 'images/heartred.png')
+        x.setAttribute('src', 'images/heartred.png');
         localStorage.setItem(like_class, 0)
 
     } else if (like_attribute === 'images/heartred.png') {
-        x.setAttribute('src', 'images/heart.png')
+        x.setAttribute('src', 'images/heart.png');
 
         localStorage.setItem(like_class, 1)
     }
-    likeBtn = document.querySelector(".like_btn")
+    likeBtn = document.querySelector(".like_btn");
 
 
 
--- final/js/weather_api.js
+++ final/js/weather_api.js
@@ -1,8 +1,18 @@
-const currentTemp = document.querySelector('#current-temp');
+const currentTemp = document.querySelector('#current_temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const wind_chill = document.querySelector('#wind_chill');
 const wind_speed = document.querySelector('#wind_speed');
+const humidity = document.querySelector('.humid');
+const day1date = document.querySelector('.day1_date');
+const day2date = document.querySelector('.day2_date');
+const day3date = document.querySelector('.day3_date');
+const day1tempMax = document.querySelector('.day1_tempMax');
+const day1tempMin = document.querySelector('.day1_tempMin');
+const day2tempMax = document.querySelector('.day2_tempMax');
+const day2tempMin = document.querySelector('.day2_tempMin');
+const day3tempMax = document.querySelector('.day3_tempMax');
+const day3tempMin = document.querySelector('.day3_tempMin');
 
 const url = "//api.openweathermap.org/data/2.5/forecast?lat=41.73549&lon=-111.83439&appid=d56761c27fa0949de266aca0d26ac7d8";
 
@@ -28,35 +38,66 @@ async function apiFetch(apiURL) {
 function displayResults(weatherData) {
     currentTemp.innerHTML = weatherData.list[0].main.temp.toFixed(1);
     kelvin = weatherData.list[0].main.temp.toFixed(1);
-    kel_to_fahr = (kelvin− 273.15) * ×9 / 5 + 32 = 87.53° F
-    const preDesc = weatherData.weather[0].description.split(" ");
+    kel_to_fahr = (kelvin - 273.15) * (9 / 5) + 32;
+    console.log(kel_to_fahr);
+    currentTemp.innerHTML = kel_to_fahr.toFixed(2);
+    humidity_number = weatherData.list[0].main.humidity;
+    humidity.innerHTML = `${humidity_number}%`;
 
+    const preDesc = weatherData.list[1].weather[0].description.split(" ");
     for (let i = 0; i < preDesc.length; i++) {
         preDesc[i] = preDesc[i][0].toUpperCase() + preDesc[i].substr(1);
+        if (preDesc[i].substr(1) === ",") {
+            preDesc[i].substr(1) = preDesc[i].substr(1) + " "
+        }
     }
-
     preDesc.join(" ")
 
-    const imagesrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const imagesrc = `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`;
     const desc = preDesc
     weatherIcon.setAttribute('src', imagesrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.innerHTML = desc;
     wind_chill_calculation = "hi";
-    document.getElementById("wind_speed").innerHTML = weatherData.wind.speed + " mph"
+    document.getElementById("wind_speed").innerHTML = weatherData.list[0].wind.speed + " mph"
 
 
-    let t = weatherData.main.temp.toFixed(1);
-    let s = weatherData.wind.speed;
-    if (t <= 50 && s >= 3) {
-        chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + ((0.4275 * t) * (Math.pow(s, .16)));
+    // let t = weatherData.main.temp.toFixed(1);
+    // let s = weatherData.wind.speed;
+    // if (t <= 50 && s >= 3) {
+    //     chillFactor = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + ((0.4275 * t) * (Math.pow(s, .16)));
 
-        chillFinal = Math.round(chillFactor * 10) / 10;
+    //     chillFinal = Math.round(chillFactor * 10) / 10;
 
-        document.querySelector("#wind_chill").innerHTML = " " + chillFinal + " F"
+    //     document.querySelector("#wind_chill").innerHTML = " " + chillFinal + " F"
 
 
-    } else {
-        document.querySelector("#wind_chill").innerHTML = " " + "N/A";
-    }
+    // } else {
+    //     document.querySelector("#wind_chill").innerHTML = " " + "N/A";
+    // }
+
+
+    let weather_date = new Date();
+    day1_date = weather_date
+    let day_two_weather_date = weather_date + 1;
+    // let
+    // let day_two_weather_date.setDate(weather_date.getDate() + 1)
+    // day_three_weather_date.setDate(day_two_weather_date.getDate() + 1)
+
+
+    day1_kelvin_min = weatherData.list[0].main.temp_min;
+    day1_kelvin_max = weatherData.list[0].main.temp_max;
+    day1_temp_min = (day1_kelvin_min - 273.15) * (9 / 5) + 32;
+    day1_temp_max = (day1_kelvin_max - 273.15) * (9 / 5) + 32;
+
+    day2_kelvin_min = weatherData.list[1].main.temp_min;
+    day2_kelvin_max = weatherData.list[1].main.temp_max;
+    day2_temp_min = (day2_kelvin_min - 273.15) * (9 / 5) + 32;
+    day2_temp_max = (day2_kelvin_max - 273.15) * (9 / 5) + 32;
+
+    day3_kelvin_min = weatherData.list[2].main.temp_min;
+    day3_kelvin_max = weatherData.list[2].main.temp_max;
+    day3_temp_min = (day3_kelvin_min - 273.15) * (9 / 5) + 32;
+    day3_temp_max = (day3_kelvin_max - 273.15) * (9 / 5) + 32;
+
 }
